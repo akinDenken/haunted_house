adab7592032670e1fbf08d47bd1fd6ec0fed2377
akinDenken <122811217+akinDenken@users.noreply.github.com>
2025-04-20 16:15:09 -0300

    Create haunted house scene with walls, roof, door and bushes

--- src/main.js
+++ src/main.js
@@ -19,12 +19,82 @@ const scene = new THREE.Scene();
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(1, 32, 32),
-  new THREE.MeshStandardMaterial({ roughness: 0.7 }),
+
+// Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({}),
 );
-scene.add(sphere);
+scene.add(floor);
+floor.rotation.x = -Math.PI * 0.5;
+
+const houseGroup = new THREE.Group();
+scene.add(houseGroup);
+
+// Walls
+const walls = new THREE.Mesh(
+  new THREE.BoxGeometry(4, 2.5, 4),
+  new THREE.MeshStandardMaterial({}),
+);
+// console.log(walls.geometry.parameters.height);
+walls.position.y += walls.geometry.parameters.height / 2;
+houseGroup.add(walls);
+
+// Roof
+const roof = new THREE.Mesh(
+  new THREE.ConeGeometry(3.5, 1.5, 4),
+  new THREE.MeshStandardMaterial({}),
+);
+roof.position.y +=
+  walls.geometry.parameters.height + roof.geometry.parameters.height / 2;
+roof.rotation.y = Math.PI * 0.25;
+houseGroup.add(roof);
+
+// Door
+const door = new THREE.Mesh(
+  new THREE.PlaneGeometry(2.2, 2.2),
+  new THREE.MeshStandardMaterial({ color: "#ff0000" }),
+);
+// console.log(walls.geometry.parameters);
+door.position.z = walls.geometry.parameters.depth / 2 + 0.001;
+door.position.y = door.geometry.parameters.height / 2;
+houseGroup.add(door);
+
+// Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial({ color: "#00ff00" });
+
+const bushPositions = [
+  [0.8, 0.2, 2.2],
+  [1.4, 0.1, 2.1],
+  [-0.8, 0.1, 2.2],
+  [-1, 0.05, 2.6],
+];
+const bushScales = [0.5, 0.25, 0.4, 0.15];
+for (let i = 0; i < bushPositions.length; i++) {
+  const bush = new THREE.Mesh(bushGeometry, bushMaterial);
+  bush.scale.setScalar(bushScales[i] ?? 0.5);
+  bush.position.set(...bushPositions[i]);
+  houseGroup.add(bush);
+}
+
+/*
+// Even more concise way of doing this: array of objects, use forEach, spread positions, and have default scale
+const bushes = [
+  { position: [0.8, 0.2, 2.2], scale: 0.5 },
+  { position: [1.4, 0.1, 2.1], scale: 0.25 },
+  { position: [-0.8, 0.1, 2.2], scale: 0.4 },
+  { position: [-1, 0.05, 2.6] }, // No scale provided
+  { position: [1.2, 0.1, 2.4] }, // New bush, no scale
+];
+
+bushes.forEach(({ position, scale = 0.5 }) => {
+  const bush = new THREE.Mesh(bushGeometry, bushMaterial);
+  bush.scale.setScalar(scale); // Defaults to 0.5 if scale is missing
+  bush.position.set(...position);
+  houseGroup.add(bush);
+});
+* */
 
 /**
  * Lights
